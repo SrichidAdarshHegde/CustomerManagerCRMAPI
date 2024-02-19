358de917cd379b77e561b1382341b3371b793572
jobs-zone <adarsh@blocdriveprivateltd.onmicrosoft.com>
2024-02-19 09:57:13 +0530

    Adi 19-02-24

--- FPL.Api/Controllers/Masters/RequestsController.cs
+++ FPL.Api/Controllers/Masters/RequestsController.cs
@@ -100,9 +100,11 @@ namespace FPL.Api.Controllers.Masters
             try
             {
                 var dat = await Task.Run(() => db.Table_Requests.Where(c => c.RequestsId == data1.RequestsId).FirstOrDefault());
+                var priid = Convert.ToInt32(data1.Priority);
+                var priData = await Task.Run(() => db.Table_Priority.Where(c => c.ID == priid).Select(c => c.PriorityID).FirstOrDefault());
                 if (dat != null)
                 {
-                    dat.Priority = data1.Priority;
+                    dat.Priority = priData;
                              
                     await Task.Run(() => db.Entry(dat).State = EntityState.Modified);
                     await db.SaveChangesAsync();
